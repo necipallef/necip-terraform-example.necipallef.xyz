b2ccab500dd68d409cd102263d1c4d8fbc7eebde
Necip Allef <necipallef@gmail.com>
2024-07-18 15:33:09 +0300

    feat: use fingerprint v0.5.0

--- fingerprint.tf
+++ fingerprint.tf
@@ -1,9 +1,9 @@
 module "fingerprint_cloudfront_integration" {
-  source = "git@github.com:necipallef/terraform-module-proxy-lambda.git/?ref=v0.4.0"
+  source = "git@github.com:necipallef/terraform-module-proxy-lambda.git/?ref=v0.5.0"
 
   fpjs_agent_download_path = "agent"
-  fpjs_get_result_path = "result"
-  fpjs_pre_shared_secret = "secret"
+  fpjs_get_result_path     = "result"
+  fpjs_shared_secret       = "secret"
 }
 
 locals {
